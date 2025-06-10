f0d1c15e5cb595e943db071ed99d9b300cfc73e2
NovaSyncc <grandiosherhaji@gmail.com>
2025-06-11 00:06:33 +0300

    Update Home component structure and layout

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import HeroSection from './Sections/HeroSection/HeroSection';
-import Countdown from './Sections/Countdown/Countdown';
+
 import HotelListings from './Sections/HotelListings/HotelListings';
 import WhyBookUs from './Sections/WhyBookUs/WhyBookUs';
 import TestimonialSection from './Sections/Testimonials/Testimonials';
@@ -10,7 +10,8 @@ const Home = () => {
   return (
     <>
       <HeroSection />
-      <Countdown />
+
+
       <HotelListings />
       <WhyBookUs />
       <TestimonialSection />
